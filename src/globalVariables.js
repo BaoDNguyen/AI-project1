33962b04770a05eb5060855f01ba34a0480ab804
BaoDNguyen <54602191+BaoDNguyen@users.noreply.github.com>
2020-10-13 10:24:14 -0500

    fix error move in the final

--- src/globalVariables.js
+++ src/globalVariables.js
@@ -6,6 +6,22 @@ for (let i = 0; i < 8; i++) {
         if (i===7) board[i][j] = 'B';    // black pieces
         if (i===0) board[i][j] = 'W';   // white pieces
         if (i > 0 && i < 7) board[i][j] = 'blank';      // blank squares
+        // if (i===0 && j===1) board[i][j] = 'W';
+        // else if (i===0 && j===5) board[i][j] = 'W';
+        // else if (i===0 && j===6) board[i][j] = 'W';
+        // else if (i===1 && j===2) board[i][j] = 'W';
+        // else if (i===1 && j===5) board[i][j] = 'B';
+        // else if (i===2 && j===1) board[i][j] = 'B';
+        // else if (i===2 && j===6) board[i][j] = 'B';
+        // else if (i===3 && j===0) board[i][j] = 'W';
+        // else if (i===3 && j===2) board[i][j] = 'B';
+        // else if (i===3 && j===4) board[i][j] = 'W';
+        // else if (i===4 && j===5) board[i][j] = 'W';
+        // else if (i===5 && j===1) board[i][j] = 'W';
+        // else if (i===5 && j===4) board[i][j] = 'B';
+        // else if (i===6 && j===0) board[i][j] = 'B';
+        // else if (i===7 && j===7) board[i][j] = 'B';
+        // else board[i][j] = 'blank';
     }
 }
 
@@ -16,7 +32,7 @@ let opponentColor = 'B';
 
 let plyDepth = 4;
 
-let heuristicConstant = 2;
+let heuristicConstant = 5;
 
 let inputMove = {};
 
